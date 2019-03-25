6fe3ce7be8e5e67d5dc434901117811b21a63080
J <jehlum11@gmail.com>
2019-03-25 11:34:59 +0530

    2D Arrays and Nested loops

---
+++ 2Darray.cpp
@@ -0,0 +1,19 @@
+//
+// Created by JP on 2019-03-25.
+//
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numberGrid[5][3] = { {10,20} , {30,40} , {50,60} , {70,80} , {90,100}};
+    for ( int i = 0; i <= 4; i++ )
+    {
+        for ( int j = 0; j <= 1; j++)
+        {
+            cout<<numberGrid[i][j]<<endl;
+        }
+
+    }
+
+}
